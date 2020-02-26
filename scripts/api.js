f0ecc58036788231d98c0b36d81c8cf6e0aa10db
NeKz <nekzor@users.noreply.github.com>
2020-02-26 19:07:46 +0100

    Create api folder if it does not exist

--- scripts/api.js
+++ scripts/api.js
@@ -4,6 +4,11 @@ const { apiFolder } = require('./config');
 const { log } = require('./utils');
 
 class Api {
+    constructor() {
+        const profileRoute = path.join(apiFolder, '/profile/');
+        if (!fs.existsSync(apiFolder)) fs.mkdirSync(apiFolder);
+        if (!fs.existsSync(profileRoute)) fs.mkdirSync(profileRoute);
+    }
     export(route, data) {
         fs.writeFileSync(path.join(apiFolder, route) + '.json', JSON.stringify({ data }));
         log.info(`exported ${route}`);
