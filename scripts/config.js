737dfbd14825e6c91a804cf2aeaa2c5cd418fa2c
NeKz <nekzor@users.noreply.github.com>
2020-06-14 19:06:47 +0200

    Delete obsolete files

--- scripts/config.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-
-module.exports = {
-    apiFolder: path.join(__dirname, '/../api'),
-    cacheFolder: path.join(__dirname, '/../cache'),
-    maxFetchRank: 5000, // max 5000 per request
-    maxBoardRank: 100, // max 100 per request
-};
