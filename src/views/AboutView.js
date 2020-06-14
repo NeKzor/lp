889907e462452c9b4f6aee4eccee9bdc1f6d6adc
NeKz <nekzor@users.noreply.github.com>
2020-06-15 01:46:52 +0200

    Update about page

--- src/views/AboutView.js
+++ src/views/AboutView.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import moment from 'moment';
-import CircularProgress from '@material-ui/core/CircularProgress';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormGroup from '@material-ui/core/FormGroup';
 import Grid from '@material-ui/core/Grid';
 import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
 import Switch from '@material-ui/core/Switch';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
 import Tooltip from '@material-ui/core/Tooltip';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
@@ -27,15 +21,9 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-const branches = [
-    { repo: 'NeKzor/lp', branch: 'master' },
-    { repo: 'NeKzBot/lp', branch: 'api' },
-    { repo: 'NeKzor/lp', branch: 'gh-pages' },
-];
-
-const noWrap = { whiteSpace: 'nowrap' };
-const MinTableCell = (props) => <TableCell size="small" {...props} />;
 const Padding = () => <div style={{ paddingTop: '50px' }} />;
+
+let clockTimer = null;
 
 const AboutView = () => {
     const isMounted = useIsMounted();
@@ -45,35 +33,29 @@ const AboutView = () => {
         dispatch,
     } = React.useContext(AppState);
 
-    const [gitHub, setGitHub] = React.useState([]);
+    const [nextUpdate, setNextUpdate] = React.useState('...');
     const classes = useStyles();
 
     React.useEffect(() => {
-        const anyErrors = (err) => {
-            console.error(err);
-            if (isMounted.current) {
-                setGitHub(undefined);
-            }
-        };
+        clockTimer = setInterval(() => {
+            // Not sure if this is right lol
+            const now = moment.utc();
+            let updateIn = moment().utc().endOf('day').add(30, 'minutes');
 
-        Promise.all(branches.map(({ repo, branch }) => fetch(`https://api.github.com/repos/${repo}/commits/${branch}`)))
-            .then((results) => {
-                Promise.all(results.map((res) => res.json()))
-                    .then((branches) => {
-                        if (isMounted.current) {
-                            setGitHub(
-                                branches.map((branch) => ({
-                                    sha: branch.sha,
-                                    author: branch.author ? branch.author : branch.commit.author,
-                                    message: branch.commit.message,
-                                    date: branch.commit.author.date,
-                                })),
-                            );
-                        }
-                    })
-                    .catch(anyErrors);
-            })
-            .catch(anyErrors);
+            if (updateIn.isBefore(now)) {
+                updateIn = moment().utc().endOf('day').add(1, 'day').add(30, 'minutes');
+            }
+
+            const duration = moment.duration({ from: now, to: updateIn });
+            const hours = duration.get('hours');
+            const minutes = duration.get('minutes');
+            const seconds = duration.get('seconds');
+
+            const g = (value) => (value === 1 ? '' : 's');
+            setNextUpdate(`${hours} hour${g(hours)}, ${minutes} minute${g(minutes)}, ${seconds} second${g(seconds)}`);
+        }, 1000);
+
+        return () => clearInterval(clockTimer);
     }, [isMounted]);
 
     const toggleDarkMode = () => {
@@ -106,85 +88,19 @@ const AboutView = () => {
                         <Typography variant="body1">
                             - A world record tie is required in case a map has more than 5000 ties.
                         </Typography>
-                        <Typography variant="body1">- Don't cheat.</Typography>
+                        <Typography variant="body1">
+                            - Do not cheat and always follow the{' '}
+                            <Link rel="noopener" href="http://lp.nekz.me/rules">
+                                rules
+                            </Link>
+                            .
+                        </Typography>
 
                         <Padding />
 
-                        <Typography variant="h5">Last Update</Typography>
+                        <Typography variant="h5">Next Update</Typography>
                         <br />
-                        {gitHub === undefined ? (
-                            <Typography variant="body1">Unable to fetch status from GitHub.</Typography>
-                        ) : gitHub.length === 0 ? (
-                            <CircularProgress className={classes.progress} />
-                        ) : (
-                            <div style={{ overflowX: 'auto' }}>
-                                <Table size="small">
-                                    <TableHead>
-                                        <TableRow>
-                                            <TableCell padding="default">
-                                                <Typography variant="body1">Branch</Typography>
-                                            </TableCell>
-                                            <TableCell padding="default">
-                                                <Typography variant="body1">Date</Typography>
-                                            </TableCell>
-                                            <TableCell padding="default">
-                                                <Typography variant="body1">Author</Typography>
-                                            </TableCell>
-                                            <TableCell padding="default">
-                                                <Typography variant="body1">Commit</Typography>
-                                            </TableCell>
-                                        </TableRow>
-                                    </TableHead>
-                                    <TableBody>
-                                        {gitHub.map((commit, idx) => {
-                                            const { repo, branch } = branches[idx];
-                                            return (
-                                                <TableRow tabIndex={-1} key={idx} style={noWrap}>
-                                                    <MinTableCell align="left">
-                                                        <Link
-                                                            color="inherit"
-                                                            rel="noopener"
-                                                            href={`https://github.com/${repo}/tree/${branch}`}
-                                                        >
-                                                            {branch}
-                                                        </Link>
-                                                    </MinTableCell>
-                                                    <MinTableCell align="left" style={noWrap}>
-                                                        <Tooltip title={moment(commit.date).toString()}>
-                                                            <span>{moment(commit.date).from()}</span>
-                                                        </Tooltip>
-                                                    </MinTableCell>
-                                                    <MinTableCell align="left">
-                                                        {commit.author.html_url ? (
-                                                            <Link
-                                                                color="inherit"
-                                                                rel="noopener"
-                                                                href={commit.author.html_url}
-                                                            >
-                                                                {commit.author.login}
-                                                            </Link>
-                                                        ) : (
-                                                            commit.author.name || 'n/a'
-                                                        )}
-                                                    </MinTableCell>
-                                                    <MinTableCell align="left" style={noWrap}>
-                                                        <Tooltip title={commit.message}>
-                                                            <Link
-                                                                color="inherit"
-                                                                rel="noopener"
-                                                                href={`https://github.com/${repo}/commit/${commit.sha}`}
-                                                            >
-                                                                {commit.sha}
-                                                            </Link>
-                                                        </Tooltip>
-                                                    </MinTableCell>
-                                                </TableRow>
-                                            );
-                                        })}
-                                    </TableBody>
-                                </Table>
-                            </div>
-                        )}
+                        {nextUpdate}
 
                         <Padding />
 
